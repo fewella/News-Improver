502b47bb1adf5f4b8b936cc3230fb294ee198f6b
Hannah Han <mailboxhannah@gmail.com>
2018-03-10 21:47:48 -0500

    we made art

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
+import os
 
 if __name__ == "__main__":
 	url = input("gimme dat newz ")
-	content = urlopen(url)
-	raw_html = content.read()
+	os.system("python html_to_text.py " + url)
+	
